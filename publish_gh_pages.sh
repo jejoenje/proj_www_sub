7ec2ff2332f524fdf73b804e40de6211a2b0e830
jejoenje <jeroenminderman@gmail.com>
2020-01-09 11:28:35 +0000

    Cleaning up some more

--- publish_gh_pages.sh
+++ publish_gh_pages.sh
@@ -7,22 +7,25 @@ then
 	fi
 
 	echo "Deleting old publication"
-	rm -rf public
-	mkdir public
+	rm -rf www/public
+	mkdir www/public
 	git worktree prune
-	rm -rf .git/worktrees/public/
+	rm -rf .git/worktrees/www/public/
 
 	echo "Checking out gh-pages branch into public"
-	git worktree add -B gh-pages public upstream/gh-pages
+	git worktree add -B gh-pages www/public origin/gh-pages
 
 	echo "Removing existing files"
-	rm -rf public/*
+	rm -rf www/public/*
 
 	echo "Generating site"
+	cd www
 	hugo
 
 	echo "Updating gh-pages branch"
 	cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
+  cd ..
+  cd ..
 	#echo "Pushing to github"
 	#git push --all
